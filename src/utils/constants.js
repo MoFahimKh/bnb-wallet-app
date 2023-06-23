57c4b1a63544a94483e35e9be0192f56fc7cee9b
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2023-06-23 22:40:54 +0530

    Add bnb-wallet-app

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const BABYDOGE_TOKEN = {
+  name: "DogeChain",
+  symbol: "wDOGE",
+  decimal: 18,
+  address: "0xdB9328a1531A55220B17545cb9FBfC02b0e6999d", 
+};
+
+export const CHAIN_ID = "0x61"; 
